a7436b3edf942cbb5e164ac86cabed23acd427f4
Aristeidis Stagkos <115788364+Arfifias@users.noreply.github.com>
2024-01-04 22:10:14 +0200

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+using namespace std;
+
+string name = "Arfifias";
+int age = 20;
+if(age == 20){
+    cout<<"I am a game developer and my nickname is " << name;
+}else{
+    cout<<"Wrong answer";
+}
+
+return 0;
+
+}
